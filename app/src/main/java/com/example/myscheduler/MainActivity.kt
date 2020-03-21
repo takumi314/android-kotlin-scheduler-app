63c0c11a2d07314b87670b14a8c6dc85d0544b83
takumi314 <isospin_symmetry@me.com>
2020-03-21 14:34:01 +0900

    [WIP] set LayoutManager and Adapter for RecyclerView

--- app/src/main/java/com/example/myscheduler/MainActivity.kt
+++ app/src/main/java/com/example/myscheduler/MainActivity.kt
@@ -5,9 +5,11 @@ import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import androidx.recyclerview.widget.LinearLayoutManager
 import io.realm.Realm
 
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.content_main.*
 
 class MainActivity: AppCompatActivity() {
 
@@ -19,6 +21,14 @@ class MainActivity: AppCompatActivity() {
         setSupportActionBar(toolbar)
         // Realmクラスのインスタンス取得
         realm = Realm.getDefaultInstance()
+        // 項目を直列に並べるためのレイアウトマネージャーを設定する（セルの配置方法）
+        list.layoutManager = LinearLayoutManager(this)
+        // 全てのスケジュールを取得する
+        val schedules = realm.where<Schedule>().findAll()
+        // スケジュールを渡すためのアダプアーを設定する（セルの生成/更新）
+        val adapter = ScheduleAdapter(schedules)
+        list.adapter = adapter
+
 
         fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
